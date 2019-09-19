00afd34688c3ed4e404e66327974feddf671af4b
saswat <nandasaswat1999@gmail.com>
2019-09-19 11:35:56 +0530

    Fifth File Added

---
+++ IntroToConditionalStatements.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+if __name__ == '__main__':
+    N = int(input())
+
+    if N%2 == 0:
+
+        if 2<N<5:
+
+            print('Not Weird')
+
+        elif 6<N<=20:
+
+            print('Weird')
+
+        else:
+            print('Not Weird')
+
+    if N%2 != 0:
+        print('Weird')

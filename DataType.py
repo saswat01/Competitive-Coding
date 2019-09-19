96a12e2a2310969d7614fb56ce0700d6b63c0cbd
saswat <nandasaswat1999@gmail.com>
2019-09-19 11:28:43 +0530

    Third File Added

---
+++ DataType.py
@@ -0,0 +1,15 @@
+
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+x = int(input())
+y = float(input())
+t = str(input())
+# Print the sum of both integer variables on a new line.
+print(i + x)
+# Print the sum of the double variables on a new line.
+print(d+y)
+# Concatenate and print the String variables on a new line
+print(s+t)
+# The 's' variable above should be printed first.
+

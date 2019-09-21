66ff0c86cb6e4a6aea5c6be45c8b76a1b820ef04
saswat <nandasaswat1999@gmail.com>
2019-09-21 16:12:35 +0530

    Another one

---
+++ Let'sReview.py
@@ -0,0 +1,11 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import math
+import os
+import random
+import re
+import sys
+
+for a in range (int(input())):
+    s = input()
+    print(s[::2], s[1::2])
+    #print(s[1::2]) 
